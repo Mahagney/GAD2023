39ab86669638a0ce784b0299a305538d9821ee03
SalehMahag <122275584+SalehMahag@users.noreply.github.com>
2023-05-10 20:17:34 +0300

    Functional

---
+++ src/lambda/LambdaFinal.java
@@ -0,0 +1,14 @@
+package lambda;
+
+public class LambdaFinal {
+    private int a = 10;
+    public void calculate(){
+        int b = 4;
+        b++;
+        a++;
+        Runnable r = () -> {
+            System.out.println(this.a);
+        };
+        r.run();
+    }
+}
